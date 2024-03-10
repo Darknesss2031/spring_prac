c11b8c632ad51da5f231e2b5cf45202b94bea8c4
Ilia <mulendeev_2003@list.ru>
2024-03-10 22:25:18 +0300

    Added custom jgsbat

--- 20240304/1/prog.py
+++ 20240304/1/prog.py
@@ -1,5 +1,6 @@
 import cowsay
 import sys
+from io import StringIO
 
 # class Monster:
 #     def __init__(self, x, y, greet):
@@ -22,10 +23,32 @@ class Player:
 
 
 monsters = {}
-def encounter(x, y):
-    print(cowsay.cowsay(*monsters[(x, y)]))
+
+jgsbat = cowsay.read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs     __\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
 
 player = Player(0, 0)
+custom_cows = {}
+custom_cows['jgsbat'] = jgsbat
+
+def encounter(x, y):
+    if monsters[(x, y)][1] in custom_cows.keys():
+        print(cowsay.cowsay(monsters[(x, y)][0], cowfile=custom_cows[monsters[(x, y)][1]]))
+        return
+    print(cowsay.cowsay(*monsters[(x, y)]))
 
 while True:
     inp = ''
@@ -62,12 +85,12 @@ while True:
                 encounter(*coords)
         case 'addmon':
             try:
-                command[3] = int(command[1])
+                command[3] = int(command[3])
                 command[2] = int(command[2])
             except:
                 print('Invalid arguments')
                 continue
-            if command[3] < 0 or command[3] > 9 or command[2] < 0 or command[2] > 9 or len(command) != 5 or command[1] not in cowsay.list_cows(): print('Invalid arguments')
+            if command[3] < 0 or command[3] > 9 or command[2] < 0 or command[2] > 9 or len(command) != 5 or (command[1] not in cowsay.list_cows() and command[1] not in custom_cows.keys()): print('Invalid arguments')
             else:
                 existed = False
                 if (command[2], command[3]) in monsters.keys(): existed = True
