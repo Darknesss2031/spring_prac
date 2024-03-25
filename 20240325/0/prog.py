ace7002fc99cf140a53e26711908f6dcd9562668
Ilia <mulendeev_2003@list.ru>
2024-03-25 14:30:45 +0300

    Classwork 25.03

---
+++ 20240325/0/prog.py
@@ -0,0 +1,46 @@
+# import sys
+# import socket
+#
+# host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
+# port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((host, port))
+#     while msg := sys.stdin.buffer.readline():
+#         s.sendall(msg)
+#         print(s.recv(1024).rstrip().decode())
+
+# import asyncio
+#
+# clients = {}
+#
+# async def chat(reader, writer):
+#     me = "{}:{}".format(*writer.get_extra_info('peername'))
+#     print(me)
+#     clients[me] = asyncio.Queue()
+#     send = asyncio.create_task(reader.readline())
+#     receive = asyncio.create_task(clients[me].get())
+#     while not reader.at_eof():
+#         done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+#         for q in done:
+#             if q is send:
+#                 send = asyncio.create_task(reader.readline())
+#                 for out in clients.values():
+#                     if out is not clients[me]:
+#                         await out.put(f"{me} {q.result().decode().strip()}")
+#             elif q is receive:
+#                 receive = asyncio.create_task(clients[me].get())
+#                 writer.write(f"{q.result()}\n".encode())
+#                 await writer.drain()
+#     send.cancel()
+#     receive.cancel()
+#     print(me, "DONE")
+#     del clients[me]
+#     writer.close()
+#     await writer.wait_closed()
+#
+# async def main():
+#     server = await asyncio.start_server(chat, '0.0.0.0', 1337)
+#     async with server:
+#         await server.serve_forever()
+#
+# asyncio.run(main())
